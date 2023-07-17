b43b991a440ff614f680099534f1242e582d491e
tpranayr <136535988+tpranayr@users.noreply.github.com>
2023-07-16 22:39:04 -0500

    Update ex_5_2.py
    complete

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -1,6 +1,6 @@
+
 """ ex_5_2.py
 This module contains an entry point that
-
 - loads data from a file `ex_5_2-data.csv` into a numpy array
 - shifts and scales the data such that the resulting mean
         is 0 and the standard deviation is 1.
@@ -15,12 +15,15 @@ except ImportError:
 
 
 if __name__ == "__main__":
-
+        
+    import os
     # Use these predefined input / output files
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
-
-    # Complete the data processing steps using numpy here.
-
-    # Save the output to OUTFILE using numpy routines.
+    raw_data = np.loadtxt(INFILE)
+    normed = (raw_data - raw_data.mean(axis=0)) / raw_data.std(axis=0)
+    processed = normed
+    os.makedirs(root_dir / "outputs", exist_ok=True)
+    np.savetxt(OUTFILE, processed, fmt='%.2e')
+	
